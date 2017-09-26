fcc6c66ff87d5640cc462ce31ef001a1ecc0bfab
Sam <sgg314@nyu.edu>
2017-09-26 14:09:12 -0400

    Finished the Rube goldberg Machine

---
+++ Assets/finalTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI; 
+
+public class finalTrigger : MonoBehaviour {
+
+	public Text winText;		// The text space that will display "You Won".
+
+	void OnTriggerEnter (Collider other)
+	{
+		Debug.Log ("Object entered the trigger");
+		winText.text = "THE END"; 
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		Debug.Log ("Object is in the trigger");
+		winText.text = "THE END"; 
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		Debug.Log ("Object exited the trigger");
+	}
+}
---
+++ Assets/startScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startScript : MonoBehaviour {
+
+	public GameObject bucket;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Destroy(bucket);
+		}
+	}
+}
